732d3f504fab01fa4360bb017b15f757dbaa78a8
optimus <optimus@MEGATRON.localdomain>
2022-09-01 02:34:56 +0100

    sixth commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdb1
+export POSTGRES_PASSWORD=Passw0rd123456.
+export POSTGRES_HOST=udagramdb1.cvgpqqu728wv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb1
+export AWS_BUCKET=udagram-bucket-1
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
